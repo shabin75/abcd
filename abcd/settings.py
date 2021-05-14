19118b2c56045379bb9e4de451f0bff35471dece
shabin75 <shabin75@gmail.com>
2021-05-14 12:42:30 +0530

    14-5-21

--- abcd/settings.py
+++ abcd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j%g_hepp@kuk*grj-8wxy&gkro#b^y7t6(y@3--5q$4-!hv(qe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
